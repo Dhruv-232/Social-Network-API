d7d16a8a30e4efa001c5f7ce926eb2159b373994
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-03 19:56:43 +1000

    Add reaction done nearly done

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -91,5 +91,25 @@ module.exports = {
             res.status(500).json(err);
           },
         
+        // Add reaction
+        async addReaction(req, res) {
+            try {
+              const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $addToSet: { reactions: req.body } },
+                { runValidators: true, new: true }
+              );
+        
+              if (!thought) {
+                return res.status(404).json({ message: 'No thought with that id!' });
+              }
+        
+              res.json(thought);
+            } catch (err) {
+              console.log(err);
+              res.status(500).json(err);
+            }
+          },
+        
         
 };
