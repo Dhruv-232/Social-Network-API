83d70d4ad44a8648cc0b4be93cec090f96c979bd
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-05-03 19:36:36 +1000

    Get single thought is done

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -15,4 +15,18 @@ module.exports = {
           return res.status(500).json(err);
         });
     },
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.Id })
+          .then(async (thought) =>
+            !thought
+              ? res.status(404).json({ message: 'No thought with that ID' })
+              : res.json({
+                thought
+              })
+          )
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+      },
 };
